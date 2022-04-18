40fcb234b12c5bfa3177af508acb2ed10d047ba6
Ng eng chun <engchun@live.com.sg>
2022-04-18 23:19:27 +0800

    added documentation
    nicely lovely..

--- engch/src/Engch/Library.java
+++ engch/src/Engch/Library.java
@@ -9,22 +9,33 @@ import java.nio.ByteBuffer;
 import java.security.SecureRandom;
 
 public class Library {
-//     import Engch.Library;
-//     Library ec = new Library();
-    private String banner = "Welcome to Engch Library!";
-    
-    public double NanoTimeSeedSecureRandom(double range) {  
-        // return a random double using system time in nano second as seed for SecureRandom();
-        // https://stackoverflow.com/questions/29273498/getting-date-time-in-unix-time-as-byte-array-which-size-is-4-bytes-with-java
-        // https://www.baeldung.com/java-secure-random
+    /**
+     * <p> import Engch.Library; </p>
+     * <p> Library ec = new Library(); </p>
+     */
+    private final String banner = "Welcome to Engch Library!";
+
+    /**
+     * <p> return a random double using system time in nano second as seed for SecureRandom(); </p>
+     * <p> references below: </p>
+     * <p><a href="https://stackoverflow.com/questions/29273498/getting-date-time-in-unix-time-as-byte-array-which-size-is-4-bytes-with-java">
+     * getting-date-time-in-unix-time-as-byte-array-which-size-is-4-bytes-with-java </a></p>
+     * <p><a href="https://www.baeldung.com/java-secure-random">
+     * java-secure-random </a></p>
+     */
+    public double NanoTimeSeedSecureRandom(double range) {
         byte[] nano_time_byte_seed = ByteBuffer.allocate(4).putInt((int) (System.nanoTime())).array();  // [int, int, int, int]
         SecureRandom rand = new SecureRandom(nano_time_byte_seed); // seeded secure random.
         return rand.nextDouble(range);  // return (double) range(0 to range)
     }
 
+    /**
+     * <p> rescaling of imageIcon </p>
+     * <p> references below: </p>
+     * <p><a href="https://stackoverflow.com/questions/6714045/how-to-resize-jlabel-imageicon">
+     * how-to-resize-jlabel-imageicon </a></p>
+     */
     public ImageIcon ImageIconScales(String imagePath, int width, int length) throws IOException {
-        // rescaling of imageIcon
-        // https://stackoverflow.com/questions/6714045/how-to-resize-jlabel-imageicon
         if (ImageIO.read(new File(imagePath)) == null) throw new IOException();  // check if image file path exist
         else {
             Image image = new ImageIcon(imagePath).getImage();  // convert imageIcon to Image
